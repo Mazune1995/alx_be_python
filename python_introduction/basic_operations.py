16e5b1999c5c7d13fccd2fa280b4aa6e32378d65
root <1995mariamnasser@gmail.com>
2025-05-27 02:35:04 -0700

    basic_operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+#!/user/bin/python3
+
+number1 = 10
+number2 = 5
+
+#Arithmetics
+sum_result = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {difference}")
+print(f"Multiplication of {number1} and {number2} is {product}")
+#
