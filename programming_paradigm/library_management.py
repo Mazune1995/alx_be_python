9f792ee068111c65e88646bb52e0252ad0b35575
Mazune1995 <1995nasser@gmail.com>
2025-07-11 11:45:13 -0700

    Files

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# library_management.py
+
+class Book:
+    """Represents a single book in the library."""
+
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        """Mark the book as checked out."""
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        """Mark the book as returned (available)."""
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
+
+    def is_available(self):
+        """Check if the book is available for checkout."""
+        return not self._is_checked_out
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+
+class Library:
+    """Represents a library that man
+#
